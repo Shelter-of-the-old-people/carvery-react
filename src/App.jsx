74f6b329ac806e776e5bfe7163b935d7d238a13d
DG <ehd666@kumoh.ac.kr>
2025-05-10 18:11:57 +0900

    fixing 404 error: not fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div class="gnb">
-        <div class="logo-frame"><div class="logo"><a href="/home">Carvery</a></div></div>
+        <div class="logo-frame">
+          <div class="logo">
+            <a href="/">Carvery</a>
+          </div>
+        </div>
         <div class="menu-frame">
           <div class="menu"><div class="menu-text"><a href="">날씨</a></div></div>
           <div class="menu"><div class="menu-text"><a href="">세차장</a></div></div>
@@ -19,7 +20,6 @@ function App() {
         </div>
         <div class="search-bar-frame">
           <form class="gnb-search-bar-frame" action="" method="get">
-            <object type="image/svg+xml" data="/icons/search.svg"></object>
             <input class="search-input" type="text"/>
           </form>
         </div>
