e462bf369addc049a9a89478e90a536f390f29e0
DG <ehd666@kumoh.ac.kr>
2025-05-12 17:10:33 +0900

    add Pages-NotFound.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/index.css';
 import App from './App.jsx';
+import Home from './routes/pages/Home.jsx';
+import NotFound from './routes/pages/NotFound.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
