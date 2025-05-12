728e597f831cc93b5915c49defd32c7ae167fe77
TTOCHIwas <95687307+TTOCHIwas@users.noreply.github.com>
2025-05-12 16:37:28 +0900

    assets

---
+++ src/components/Gnb.jsx
@@ -0,0 +1,17 @@
+import React,{use, useState} from 'react';
+
+const Gnb = () => {
+
+    return  (<div class="gnb">
+            <div class="logo-frame"><div class="logo"><a href="/home">Carvery</a></div></div>
+            <div class="menu-frame">
+                <div class="menu"><div class="menu-text"><a href="">{menu}</a></div></div>
+            </div>
+            <div class="search-bar-frame">
+                <form class="gnb-search-bar-frame" action="" method="get">
+                    <object type="image/svg+xml" data="../assets/search.svg"></object>
+                    <input type="text" name='search' class="search-input" />
+                </form>
+            </div>
+        </div>);
+};
