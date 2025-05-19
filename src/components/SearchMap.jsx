7a3f112213eb8a851dfd54a63583130dc3865ea3
TTOCHIwas <95687307+TTOCHIwas@users.noreply.github.com>
2025-05-19 22:24:13 +0900

    중간 commit

--- src/components/SearchMap.jsx
+++ src/components/SearchMap.jsx
@@ -3,10 +3,12 @@ import SearchBar from "./SearchBar";
 
 function SearchMap() {
   return (
-    <div className="home">
-      <h2 className="search-map-title">지도로 찾기</h2>
+    <div className="map-frame">
+      <div className="title-frame">
+        <h2 className="search-map-title">지도로 찾기</h2>
         <SearchBar />
-        <LeafletMap className="map-box"latitude={37.5665} longitude={126.9780} />
+      </div>
+        <LeafletMap latitude={37.5665} longitude={126.9780} />
       {/* 서울시청 좌표 */}
     </div>
   );
