6825b87024877153ff9c5a5f0993c9321d5f3f05
TTOCHIwas <95687307+TTOCHIwas@users.noreply.github.com>
2025-05-19 12:01:55 +0900

    GNB 추가, globals.css 추가, HomeDevelop.jsx 수정

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,20 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import WeeklyList from './WeeklyList';
-import '../styles/styleguide.css';
-import '../styles/globals.css';
-import '../styles/Weather.css';
 
 const mockWeather =
     {
       weatherToday: {type: 'sunny', temp: '24'},
       weatherWeeks: [
-        {weather: 'sunny', temperature: '1', day: '월요일'},
-        {weather: 'rainy', temperature: '2', day: '화요일'},
-        {weather: 'cloudy', temperature: '3', day: '수요일'},
-        {weather: 'windy', temperature: '4', day: '목요일'},
-        {weather: 'snowy', temperature: '5', day: '금요일'},
-        {weather: 'sunny', temperature: '6', day: '토요일'},
-        {weather: 'cloudy', temperature: '7', day: '일요일'},
+        {weather: 'sunny', temperature: '11', day: '월요일'},
+        {weather: 'rainy', temperature: '22', day: '화요일'},
+        {weather: 'cloudy', temperature: '33', day: '수요일'},
+        {weather: 'windy', temperature: '44', day: '목요일'},
+        {weather: 'snowy', temperature: '55', day: '금요일'},
+        {weather: 'sunny', temperature: '66', day: '토요일'},
+        {weather: 'cloudy', temperature: '77', day: '일요일'},
       ],
       date: '2025년 5월 18일',
       location: '경상북도 구미시 거의동'
