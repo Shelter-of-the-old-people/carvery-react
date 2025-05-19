900069c3282c806cbaff6de952c08c79d3707068
comboong2 <mhtiger1014@naver.com>
2025-05-19 11:05:27 +0900

    sda

--- src/routes/pages/Home.jsx
+++ src/routes/pages/Home.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import ProductList from '../../components/ProductList';
-<<<<<<< HEAD
-import FacilityList from '../../components/FacilityList';
-=======
 import Weather from '../../components/Weather';
 import '../../styles/styleguide.css';
 import '../../styles/Home.css';
->>>>>>> main
 
 const Home = () => {
   return (
     <div style={{ padding: '20px' }}>
       <h2>추천 세차용품</h2>
-<<<<<<< HEAD
       <ProductList />
       <h2>관광지</h2>
       <FacilityList/>
-=======
       <Weather />
->>>>>>> main
     </div>
   );
 };
