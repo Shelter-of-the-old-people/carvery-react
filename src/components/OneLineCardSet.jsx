2bbd3113281062124019c3913042d05ee1411aae
BamSongE <soog2530@naver.com>
2025-06-21 21:42:36 +0900

    Map파일에서 carWashList설정

--- src/components/OneLineCardSet.jsx
+++ src/components/OneLineCardSet.jsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useState, useRef } from 'react';
 import FacilityList from './FacilityList';
-import { fetchNearbyData } from '../api/NearbyApi';
+  import { fetchNearbyData } from '../api/NearbyApi';
 
 const OneLineCardSet = ({title, lat, lng, carWashData}) => { 
-    const listRef = useRef(null);
-    const [data, setData] =useState([]);
-    const [scrolling, setScrolling] = useState(false);
+  const listRef = useRef(null);
+  const [data, setData] =useState([]);
+  const [scrolling, setScrolling] = useState(false);
 
-    useEffect(() => {
-    if (lat && lng) {
-      fetchNearbyData(lat, lng)
-        .then(setData)
-        .catch(err => console.error('API 오류:', err));
-    }
-  }, [lat, lng]);
+  //   useEffect(() => {
+  //   if (lat && lng) {
+  //     fetchNearbyData(lat, lng)
+  //       .then(setData)
+  //       .catch(err => console.error('API 오류:', err));
+  //   }
+  // }, [lat, lng]);
 
     const scrollLeft = () => {
   if (listRef.current && !scrolling) {
